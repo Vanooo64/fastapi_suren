1d0e33531fa84f081ad06d4d8e66d5178569ecc5
Vano <glazglojon@gmail.com>
2025-02-09 12:07:40 +0200

    use poetry for version control

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
-from pydantic import BaseModel, EmailStr
 
 import uvicorn
 
-app = FastAPI()
+from items_views import router as items_router
+from users.views import router as users_router
 
-class CreateUser(BaseModel):
-    email: EmailStr
+app = FastAPI()
+app.include_router(items_router)
+app.include_router(users_router)
 
 @app.get("/")
 def hello_index():
@@ -19,13 +20,6 @@ def hello(name: str= "World"):
     name = name.strip().title()
     return {"message": f"Hello {name}",}
 
-@app.post("/users/")
-def create_user(user: CreateUser):
-    return {
-        "massege": "succes",
-        "email": user.email,
-    }
-
 
 @app.post("/calc/add/")
 def add_calc(a: int, b:int):
@@ -33,25 +27,6 @@ def add_calc(a: int, b:int):
         "a": a,
         "b": b,
         "result": a + b,
-    }
-
-@app.get("/items/")
-def list_items():
-    return [
-        'Items1',
-        'Items2',
-    ]
-
-@app.get("/items/latest/")
-def get_latest_item():
-    return {"item": {"id": "0", "name": "latest"}}
-
-@app.get("/items/{item_id}/")
-def get_item_by_id(item_id: int):
-    return {
-        "item":{
-            "id": item_id,
-        }
     }
 
 if __name__ == "__main__":
---
+++ users/crud.py
@@ -0,0 +1,15 @@
+"""
+Create
+Read
+Update
+Delete
+"""
+from users.shemas import CreateUser
+
+def create_user(user_in: CreateUser) -> dict:
+    user = user_in.model_dump() #повертає словник
+    return {
+        "success": True,
+        "user": user,
+    }
+
